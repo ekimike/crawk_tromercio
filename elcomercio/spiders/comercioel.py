b13e3d59b01ab633ca003d856364503fd4fb42d5
Miguel Alcantara <miguel.alc@gmail.com>
2018-10-09 22:44:32 -0500

    Iterates over all pages

--- elcomercio/spiders/comercioel.py
+++ elcomercio/spiders/comercioel.py
@@ -1,31 +1,48 @@
 # -*- coding: utf-8 -*-
-import scrapy
+from scrapy import Spider
+from scrapy.http import Request
 
 
-class ComercioelSpider(scrapy.Spider):
+class ComercioelSpider(Spider):
     name = 'comercioel'
     allowed_domains = ['elcomercio.pe']
-    start_urls = ['https://elcomercio.pe/']
+    #start_urls = ['https://elcomercio.pe/']
+    start_urls = ['https://elcomercio.pe/buscar/?query=discapacitados']
+
 
     def parse(self, response):
-        return scrapy.FormRequest.from_response(
-            response,
-            formdata={"query":"discapacitados"},
-            callback=self.search_list_result
-        )
-
-    #We got list for first result page
-    def search_list_result(self, response):
-
+        #return scrapy.FormRequest.from_response(
+        #    response,
+        #    formdata={"query":"discapacitados"},
+        #    callback=self.search_list_result
+        #)
         boxes_url = response.xpath('//h2/a/@href').extract()
-
 
         for box in boxes_url:
             boxes_url_absolute = response.urljoin(box)
             yield {
                 'page_link': boxes_url_absolute
             }
-        #    yield Request(boxes_url_absolute)
 
+        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        self.logger.info("------------------------- %s",next_page_url)
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
+
+    #We got list for first result page
+    def search_list_result(self, response):
+
+        boxes_url = response.xpath('//h2/a/@href').extract()
+
+        for box in boxes_url:
+            boxes_url_absolute = response.urljoin(box)
+            yield {
+                'page_link': boxes_url_absolute
+            }
+
+        next_page_url = response.xpath('//li[@class="next"]/a/@href').extract_first()
+        self.logger.info("------------------------- %s",next_page_url)
+        absolute_next_page_url = response.urljoin(next_page_url)
+        yield Request(absolute_next_page_url)
 
         #next_page = response.urljoin(response.xpath('//*[@class="next"]/a/@href').extract())
